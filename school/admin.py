4e3fd6de259da67c42708c7e5d7bbfa919bfa48b
roosvelt <delanoroosvelt733@gmail.com>
2021-07-07 12:34:00 +0100

    add models in admin

--- school/admin.py
+++ school/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Matter, Classe, ClassRoom
+from .models import Lecon, Matter, Classe, ClassRoom, Program, Question, Reponse, TestResults
 
 # Register your models here.
 # admin.site.register(Speciality)
 admin.site.register(Matter)
 admin.site.register(Classe)
 # admin.site.register(Level)
+admin.site.register(Program)
+admin.site.register(Lecon)
+admin.site.register(Reponse)
 admin.site.register(ClassRoom)
+admin.site.register(Question)
+admin.site.register(TestResults)

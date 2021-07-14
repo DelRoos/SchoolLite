0722c24fa5aa441ae0b6d46937aeacae0a24f9c6
roosvelt <delanoroosvelt733@gmail.com>
2021-07-14 16:41:19 +0100

    put debug false ...

--- SchoolOnline/settings.py
+++ SchoolOnline/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*(l)yia779@thwp#gzql#0)lth-3qt98ukoybg8wuo5^dv4akp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
